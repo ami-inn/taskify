a40855ab97a895d38ec749eca5b2273900510880
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-05-20 20:16:44 +0530

    create server and modify with serverjs and helpers and middlewares

---
+++ server/config/cloudinary.js
@@ -0,0 +1,9 @@
+import cloudinary from 'cloudinary'
+
+cloudinary.v2.config({ 
+    cloud_name:'dyeor2etw' , 
+    api_key:'166131512883639', 
+    api_secret:'gNyZwTayjCKMLNh9XmOgs1Z7Ll8'
+  });
+
+export default cloudinary.v2
---
+++ server/server.js
@@ -0,0 +1,35 @@
+// import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import dbConnect from './config/dbConnect.js'
+
+
+
+
+
+
+
+
+
+
+
+const app=express()
+app.use(express.json({limit:'50mb'}))
+app.use(cookieParser)
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.resolve()+'/public'))
+app.use(
+    cors({
+        origin:[
+            'http://localhost:3000'
+        ],
+        credentials:true,
+    })
+)
+dbConnect()
+
+app.listen(5000,()=>{
+    console.log('server running on port 5000')
+})
