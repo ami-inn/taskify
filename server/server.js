51aca84e2d931d206a2dcd6bdb6e04c0c8a8b7b5
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-07-12 11:34:03 +0530

    changed the  url in app and server

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:3000",
-    //  'https://taskifi.netlify.app'
+     'https://taskifi.netlify.app'
     ],
   },
 });
@@ -42,7 +42,7 @@ app.use(
     cors({
         origin:[
             'http://localhost:3000',
-            // 'https://taskifi.netlify.app'
+            'https://taskifi.netlify.app'
         ],
         credentials:true,
     })
