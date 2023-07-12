e81d01bdcfa5bdd295d0fe31512d238725453a6f
Ameen Pk <110180459+ami-inn@users.noreply.github.com>
2023-07-12 11:36:35 +0530

    added some updates

--- server/server.js
+++ server/server.js
@@ -24,9 +24,7 @@ const app=express()
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000",
-     'https://taskifi.netlify.app'
-    ],
+    origin: ["http://localhost:3000",'https://taskifi.netlify.app'],
   },
 });
 
